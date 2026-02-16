ad044a85a64803366084c0f3506b68111d233db7
Am26-Dev <inductance2647@gmail.com>
2026-02-16 13:43:25 +0530

    new-folder

--- 01/src/App.jsx
+++ 01/src/App.jsx
@@ -46,7 +46,6 @@ const App = () => {
       {/* <TimeLineAnimNav /> */}
 
       <TextAnimation />
-
       <TextScroll />
     </>
   );
--- 02/src/components/perspective/PersTransition.jsx
+++ 02/src/components/perspective/PersTransition.jsx
@@ -8,28 +8,7 @@ export const PersTransition = () => {
             <section className="flex h-screen justify-center items-center bg-red-400">
                 <h1 className="text-center text-4xl uppercase">Perspective Section Transition</h1>
             </section>
-            {/* <PersT /> */}
-
-            <section className="flex flex-col bg-pink-400 h-screen w-full">
-                <div className="bg-blue-400 px-10 py-6">
-                    <h3 className="text-6xl font-semibold">PORTRAIT</h3>
-                </div>
-                <div className="bg-blue-700 flex flex-1 justify-baseline items-end px-10 pb-16">
-                    <div className="bg-amber-700  max-w-sm">descriptoin</div>
-
-                    <div className="bg-yellow-200 relative flex flex-col items-start">
-                        <span className="absolute -top-10 text-3xl font-semibold">number(01)</span>
-                         <div className="w-[320px] aspect-[3/4] overflow-hidden">
-                <img
-                    src="./iron1.jpg"
-                    alt=""
-                    className="w-full h-full object-cover"
-                />
-            </div>
-                    </div>
-                </div>
-            </section>
-
+            <PersT />
             <section className="flex h-screen justify-center items-center bg-red-600">
                 <h1 className="text-center text-4xl uppercase">See you next one!</h1>
             </section>
